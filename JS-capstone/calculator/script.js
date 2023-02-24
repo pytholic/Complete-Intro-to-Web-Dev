8b621707f285c82900c79923d05bf035a011e28b
pytholic <rajahaseeb147@gmail.com>
2023-02-24 16:52:47 +0900

    fixed hover colors

---
+++ JS-capstone/calculator/script.js
@@ -0,0 +1,5 @@
+const keys = document.querySelector(".div-keys");
+console.log(key);
+keys.addEventListener("click", function () {
+  document.getElementsByClassName("key").style.backgroundColor = "#333333";
+});
